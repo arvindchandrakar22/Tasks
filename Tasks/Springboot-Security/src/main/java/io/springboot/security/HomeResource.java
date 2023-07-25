efd392d1c0f0b79141a2c1b8e1ce06b38a99b0a9
Arvind <arch9805@gmail.com>
2023-07-25 15:32:39 +0530

    New Commit :)

--- Tasks/Springboot-Security/src/main/java/io/springboot/security/HomeResource.java
+++ Tasks/Springboot-Security/src/main/java/io/springboot/security/HomeResource.java
@@ -9,4 +9,12 @@ public class HomeResource {
     public String home(){
         return ("<h1>Welcome !! Lets Learn Spring Security !!</h1>");
     }
+    @GetMapping("/user")
+    public String user(){
+        return ("<h1>Hey Hello, Welcome USER !!</h1>");
+    }
+    @GetMapping("/admin")
+    public String admin(){
+        return ("<h1>Hey Hello, Welcome ADMIN !!</h1>");
+    }
 }
